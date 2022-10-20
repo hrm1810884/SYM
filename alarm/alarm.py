49f6fbaf12a4011506c4c1f6053476db61bfb4f4
nabe0501 <five0501.km@gmail.com>
2022-10-20 16:15:12 +0900

    add: get hours,minutes

--- alarm/alarm.py
+++ alarm/alarm.py
@@ -1,2 +1,13 @@
 str = input()
-print(str)
+#str = '<s>5時半にかけて</s>'
+minute = 0
+for i in range(len(str)):
+    if str[i] == '1':
+        hour = 10
+    if str[i] in ['5','6','7','8','9']:
+        hour = int(str[i])
+    if str[i] == '3' or str[i] == '半':
+        minute = 30
+
+
+    
--- alarm/asr-recog.sh
+++ alarm/asr-recog.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
 
-padsp julius -C asr/grammar-mic.jconf | ./asr-output.pl
+#padsp julius -C asr/grammar-mic.jconf | ./asr-output.pl
+padsp julius -C asr/grammar-mic.jconf | ./asr-output.pl | python3 alarm.py
 #padsp julius -C asr/grammar-mic.jconf
