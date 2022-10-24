eaa12c75e59bf77316e507e5d0f57f30bbdc2e52
takep <takepgaku@gmail.com>
2022-10-24 18:56:54 +0900

    fix: コメントを追加

--- init/init.sh
+++ init/init.sh
@@ -1,7 +1,6 @@
 #!/bin/bash
 
 FILE_INIT="./init.dat"
-DATA_DIR="./data/"
 
 clear
 echo "┏━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━┓"
@@ -12,10 +11,17 @@ echo -e "\033[0m          \033[m┃"
 echo "┃                                 ┃"
 echo "┃ Smarter than You in the Morning ┃"
 echo "┃                                 ┃"
+echo "┃    Press any key to continue    ┃"
+echo "┃                                 ┃"
 echo "┗━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━┛"
-sleep 1
+
+stty -echo
+read -r _
+stty echo
+
 clear
 
+# ファイルがある場合は本当に初期化するか確認する
 if [ -f "$FILE_INIT" ]; then
   echo -e "\033[31;1mWarning\033[m: You initializtion has already been completed."
   while :
@@ -32,16 +38,22 @@ if [ -f "$FILE_INIT" ]; then
   done
 fi
 
+# ファイルを作成"
 echo -n > "$FILE_INIT"
+
+# 名前を取得
 echo "Tell me your name"
 read -r input_name
 echo -e "name\t$input_name" > "$FILE_INIT"
+
+# 最寄り駅を取得
 echo "Tell me your nearest station（e.g. 本郷三丁目）"
 echo "Note: Don't add '駅' at the end"
 read -r input_nearest_station
 echo -e "nearest_station\t$input_nearest_station" >> "$FILE_INIT"
 
-while :
+# 都道府県を取得
+while : # 東京都って言わないと終了しない
 do
   echo "Select the prefecture you live in:"
   echo "Note: Copy from below and paste"
@@ -52,11 +64,13 @@ do
   echo ""
   read -r input_prefecture
   if [ "$input_prefecture" == "東京都" ]; then
+    echo -e "prefecture\t$input_prefecture" >> "$FILE_INIT"
     break
   fi
-  echo "I'm sorry but SYM is only for citizen of Tokyo."
+  echo -e "I'm sorry but SYM is only for citizen of Tokyo.\n"
 done
-echo -e "prefecture\t$input_prefecture" >> "$FILE_INIT"
+
+# 都市を取得
 echo "Select the nearest city from below:"
 while read -r city _
 do
