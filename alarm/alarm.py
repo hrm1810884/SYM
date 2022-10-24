5743717552eab488ba61023ef170bb272ca0db57
nabe0501 <five0501.km@gmail.com>
2022-10-24 15:39:43 +0900

    refactaring2

--- alarm/alarm.py
+++ alarm/alarm.py
@@ -2,32 +2,7 @@ import schedule
 import time
 import os
 #installしてない場合はscheduleを入れる
-#アラーム処理
-def Alarm():
-    Sound()
-    print("end")
-    exit()   #これがないと無限ループになる
-#音再生処理
-def Sound():
-    """
-    #pygameがjuliusと衝突して音がならないためos.systemに切り替え
-    print("sound")
-    pygame.mixer.init() #初期化
-    pygame.mixer.music.load('alarm1.mp3') #読み込み
-    
-    pygame.mixer.music.play(-1) #ループ再生（引数を1にすると1回のみ再生）
-    input()
-    pygame.mixer.music.stop() #終了
-    """
-    while(1): 
-        os.system("play alarm1.mp3")#パス指定必要な場合はここで
-        time.sleep(5)
-        string = input()
-        print(string)
-        if  checkStop(string):
-            return 
-
-           
+  
     
 def main():
     string = input()
@@ -39,14 +14,14 @@ def main():
         string = input()
     (hour,minute) = getTime(string)
     #test
-    #hour = 15
-    #minute = 22
+    hour = 15
+    minute = 36
     #↑にテスト用時刻入れる
 
     target = f'{str(hour).zfill(2)}:{str(minute).zfill(2)}'
     print(target+'にアラームをセットしました')
     #アラーム時間設定
-    schedule.every().day.at(target).do(Alarm)
+    schedule.every().day.at(target).do(Sound)
     #アラーム待ち
     while True:
         schedule.run_pending()    
@@ -64,8 +39,6 @@ def getTime(string):
             hour = int(string[i])
         if string[i] == '3' or string[i] == '半':
             minute = 30
-    print(hour)
-    print(minute)
     return (hour, minute)
 
 #時刻設定だったら1、止めるだったら0
@@ -79,6 +52,19 @@ def checkStop(string):
     if checkTime(string):
         return 0
     return 1
+
+#音再生処理
+def Sound():
+    while(1): 
+        os.system("play alarm1.mp3")#パス指定必要な場合はここで
+        time.sleep(5)
+        string = input()
+        print(string)
+        if  checkStop(string):
+            print("alarm stopped")
+            exit()
+
+        
 
 if __name__ == '__main__':
     main()
