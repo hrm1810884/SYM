bb069ffbd8d67df51312bb04de1f478959ca4a4b
nabe0501 <five0501.km@gmail.com>
2022-10-24 15:34:21 +0900

    refactaring

--- alarm/alarm.py
+++ alarm/alarm.py
@@ -1,14 +1,12 @@
-import pygame.mixer
 import schedule
 import time
-import sys
 import os
-#installしてない場合はpygameとscheduleを入れる
+#installしてない場合はscheduleを入れる
 #アラーム処理
 def Alarm():
-    print("alarm")
     Sound()
-    exit()   #これがないと無限ループになるので注意
+    print("end")
+    exit()   #これがないと無限ループになる
 #音再生処理
 def Sound():
     """
@@ -21,50 +19,67 @@ def Sound():
     input()
     pygame.mixer.music.stop() #終了
     """
-    while(1):
-        print("Sound")
-        os.system("play alarm1.mp3")
+    while(1): 
+        os.system("play alarm1.mp3")#パス指定必要な場合はここで
         time.sleep(5)
-        stop = input()
-        print(stop)
-        for i in range(len(stop)):
-            
-            if (stop[i] == '止'):
-                print("alarm stop")
-                return 0
+        string = input()
+        print(string)
+        if  checkStop(string):
+            return 
+
            
     
 def main():
-    #string = sys.stdin.readline()
-
     string = input()
-    print(string)
-    #str = '<s>5時半にかけて</s>'
-    minute = 0
-    #目覚まし設定時間取得
-    
-    for i in range(len(string)):
-        if string[i] == '1':
-            hour = 10
-        if string[i] in ['5','6','7','8','9']:
-            hour = int(string[i])
-        if string[i] == '3' or string[i] == '半':
-            minute = 30
+    while(True):
+        print(string)
+        if checkTime(string):
+            break
+        time.sleep(5)
+        string = input()
+    (hour,minute) = getTime(string)
     #test
-    hour = 14
-    minute = 49
+    #hour = 15
+    #minute = 22
+    #↑にテスト用時刻入れる
+
     target = f'{str(hour).zfill(2)}:{str(minute).zfill(2)}'
     print(target+'にアラームをセットしました')
     #アラーム時間設定
     schedule.every().day.at(target).do(Alarm)
     #アラーム待ち
     while True:
-        schedule.run_pending()
-        speak = input()
-        print(speak)    
-        
+        schedule.run_pending()    
         time.sleep(1)
-    
+
+
+#目覚まし設定時間取得
+def getTime(string):
+    minute = 0
+    hour = 0
+    for i in range(len(string)):
+        if string[i] == '':
+            hour = 10
+        if string[i] in ['5','6','7','8','9']:
+            hour = int(string[i])
+        if string[i] == '3' or string[i] == '半':
+            minute = 30
+    print(hour)
+    print(minute)
+    return (hour, minute)
+
+#時刻設定だったら1、止めるだったら0
+def checkTime(string):
+    for i in range(len(string)):
+        if string[i] == "止":
+            return 0
+    return 1
+
+def checkStop(string):   
+    if checkTime(string):
+        return 0
+    return 1
+
 if __name__ == '__main__':
     main()
     
