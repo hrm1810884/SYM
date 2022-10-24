d0f1b12d05021de293ba1fb1f880d6fc75b0c1c8
hrm1810884 <hrm1810884@g.ecc.u-tokyo.ac.jp>
2022-10-24 18:02:55 +0900

    fix: 返り値を適切に設定

--- fetch_calendar/fetch_calendar.py
+++ fetch_calendar/fetch_calendar.py
@@ -1,7 +1,6 @@
 import datetime
 import os
 import pickle
-import sys
 
 from google.auth.transport.requests import Request
 from google_auth_oauthlib.flow import InstalledAppFlow
@@ -27,7 +26,7 @@ def get_creds():
         # アクセストークンを要求
         else:
             flow = InstalledAppFlow.from_client_secrets_file(
-                'credentials.json', SCOPES)
+                '../fetch_calendar/credentials.json', SCOPES)
             creds = flow.run_local_server(port=0)
         # アクセストークン保存（２回目以降の実行時に認証を省略するため）
         with open('token.pickle', 'wb') as token:
@@ -35,7 +34,7 @@ def get_creds():
     return creds
 
 
-def print_txt(events):
+def get_txt(events):
     output = []
     if not events:
         output.append('今日は特別な日程はありません')
@@ -67,7 +66,8 @@ def print_txt(events):
                     + event['summary']
                     + 'があります'
                 )
-    sys.stdout.write(';'.join(output))
+    result = ', '.join(output)
+    return result
 
 
 def main():
@@ -99,7 +99,7 @@ def main():
     )
     events = events_result.get('items', [])
 
-    print_txt(events)
+    return get_txt(events)
 
 
 if __name__ == '__main__':
