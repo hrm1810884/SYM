37d1a1cf62012e26f47363122931e2b2d0a0007a
takep <takepgaku@gmail.com>
2022-10-27 03:14:19 +0900

    chore: refactor

--- alarm/alarm.py
+++ alarm/alarm.py
@@ -12,6 +12,7 @@ def main():
     # アラーム時間設定
     get_up_time = f"{str(hour).zfill(2)}:{str(minute).zfill(2)}"
     schedule.every().day.at(get_up_time).do(sound)
+
     # アラーム待ち
     while True:
         if not os.path.isfile("../alarm/alarm_set.dat"):
@@ -20,28 +21,37 @@ def main():
         time.sleep(1)
 
 
-# 目覚まし設定時間取得
-def get_time(string):
-    minute = 0
-    hour = 0
-    for char in string:
+def extract_time_from_command(command):
+    """文字列からアラームの時刻を計算する
+
+    Parameters
+    ----------
+    command : str
+        入力されたコマンド
+
+    Returns
+    -------
+    tuple
+        (アラームをセットする時, アラームをセットする分)
+    """
+    for char in command:
         if char == "1":
             hour = 10
         if char in ["5", "6", "7", "8", "9"]:
             hour = int(char)
-        if char == "3" or char == "半":
+        if char in ["3", "半"]:
             minute = 30
-    return int(hour), int(minute)
+    return hour, minute
 
 
-# 音再生処理
 def sound():
+    """音を鳴らす
+    """
     while True:
         os.system("play ../alarm/alarm1.mp3")  # パス指定必要な場合はここで
         if os.path.isfile("../alarm/alarm_set.dat"):
-            with open("../alarm/alarm_set.dat","w") as f:
-                alarm_ringed = str(1)
-                f.write(alarm_ringed)
+            with open("../alarm/alarm_set.dat", "w") as f:
+                f.write("1")
         time.sleep(1)
         if not os.path.isfile("../alarm/alarm_set.dat"):
             return schedule.CancelJob()
